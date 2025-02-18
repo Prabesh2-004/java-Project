c39ccb59885bd25ceb255847a84c48c7eaeef969
Prabesh2-004 <coder.bro2004@gmail.com>
2025-02-18 17:45:28 +0545

    loops

---
+++ Java2/src/loops.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class loops {
+    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- Java2/src/primeNumber.java
+++ Java2/src/primeNumber.java
@@ -4,9 +4,9 @@ public class primeNumber {
     public static void main(String[] args){
         System.out.print("Enter a Number: ");
         int prime = scanner.nextInt();
-        for (int i = 1; i<=prime;i++){
-            if (prime % i == 0){
-                
+        for (int i = 1; i <= prime; i++){
+            if ((i%i) !=0){
+                System.out.println(i);
             }
         }
 
